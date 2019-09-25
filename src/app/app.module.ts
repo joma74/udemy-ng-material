69a45b056f8616c086b3f882fdef1e5b90163061
joma74 <dev-mails@gmx.net>
2019-09-25 19:00:14 +0200

    chore[setup]: activate hmr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from "@angular/platform-browser"
 import { NgModule } from "@angular/core"
 
 import { AppComponent } from "./app.component"
+import { ProductComponent } from "./product/product.component"
+
+import { NgxsModule } from "@ngxs/store"
+import { environment } from "../environments/environment"
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, ProductComponent],
+  imports: [
+    BrowserModule,
+    NgxsModule.forRoot([], { developmentMode: !environment.production }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
