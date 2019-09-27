89ae62e02b4039001bbd1749270822180d38a9b6
joma74 <dev-mails@gmx.net>
2019-09-28 01:11:21 +0200

    chore(setup): installed angular cdk and material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser"
 
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 import { AppComponent } from "./app.component"
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
