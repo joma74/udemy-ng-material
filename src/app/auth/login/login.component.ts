f7c4d5de1864e24b9e3a4bf5b62e68c67ba8fbfa
joma74 <dev-mails@gmx.net>
2019-10-01 00:11:05 +0200

    feat: login form built in a template driven approach

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core"
+import { NgForm } from "@angular/forms"
 
 @Component({
   selector: "app-login",
@@ -9,4 +10,8 @@ export class LoginComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  onSubmit(f: NgForm) {
+    console.log(f)
+  }
 }
