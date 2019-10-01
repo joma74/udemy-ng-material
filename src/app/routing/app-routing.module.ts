998b01579569272fb2be854e25da2d091d787047
joma74 <dev-mails@gmx.net>
2019-10-02 00:08:08 +0200

    refactor: moved routing module into own folder

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from "@angular/core"
 import { RouterModule, Routes } from "@angular/router"
-import { LoginComponent } from "./auth/login/login.component"
-import { SignupComponent } from "./auth/signup/signup.component"
-import { TrainingComponent } from "./training/training.component"
-import { WelcomeComponent } from "./welcome/welcome.component"
+import { LoginComponent } from "../auth/login/login.component"
+import { SignupComponent } from "../auth/signup/signup.component"
+import { TrainingComponent } from "../training/training.component"
+import { WelcomeComponent } from "../welcome/welcome.component"
 
 const routes: Routes = [
   { path: "", component: WelcomeComponent },
