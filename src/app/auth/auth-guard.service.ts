8581dd9957fcd2301f574086002cef34f9d67cd3
joma74 <dev-mails@gmx.net>
2019-10-10 14:01:29 +0200

    refactor: return result of navigate to canActivate

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -17,7 +17,7 @@ export class AuthGuard implements CanActivate {
     if (this.authService.isAuth()) {
       return true
     } else {
-      this.router.navigate(["/login"])
+      return this.router.navigate(["/login"])
     }
   }
 }
