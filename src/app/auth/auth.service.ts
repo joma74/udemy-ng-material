dd8a132f112985ba08c48b15073c3f20d75e6d4b
joma74 <dev-mails@gmx.net>
2019-10-17 15:30:55 +0200

    chore: cleanup imports

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@angular/core"
 import { AngularFireAuth } from "@angular/fire/auth"
-import { MatSnackBar } from "@angular/material"
 import { Router } from "@angular/router"
 import { Subject } from "rxjs"
 import { UIService } from "../shared/ui.service"
