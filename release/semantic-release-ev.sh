740288789e44bdb08b7b6f4b4ea1a36c485925ef
joma74 <dev-mails@gmx.net>
2019-10-31 16:37:56 +0100

    chore: use stdout only for error displayment

--- release/semantic-release-ev.sh
+++ release/semantic-release-ev.sh
@@ -6,10 +6,18 @@
 # release/semantic-release-ev.sh 2> >(while read line; do echo -e "\e[01;31m$line\e[0m" >&2; done)
 #
 
->&2 echo "args: $@"
+# see https://ops.tips/gists/redirect-all-outputs-of-a-bash-script-to-a-file/
+# see https://catonmat.net/ftp/bash-redirections-cheat-sheet.pdf
+# copy fd 1(stdout) to 3
+exec 3>&1
+# copy fd 2(stdout) to 1. all stdout from now on goes to stderr
+exec 1>&2
 
-printenv 1> >(grep -i release >&2)
+echo "args: $@"
 
->&1 echo "evaluate version only, ignore expected ✖ EVERIFYRELEASE AggregateError:SemanticReleaseError"
+printenv | grep -i release
+
+# this is the error displayment, so use fd 3 
+>&3 echo "evaluate version only, ignore expected ✖ EVERIFYRELEASE AggregateError:SemanticReleaseError"
 
 exit -1
